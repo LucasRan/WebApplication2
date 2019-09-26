40dbd534272a77c9eb203119baa35eb0e2dcf4e5
Haochuan <lucasran1993@gmail.com>
2019-09-25 19:00:29 -0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product {
+            ProductId = 1,
+            Name = "kAYAK",
+            Description = "A boat for one person",
+            Price = 275M,
+            Category = "WATER SPORTS"
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
